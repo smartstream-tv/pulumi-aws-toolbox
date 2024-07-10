e9fabbe4bb9deea23fadc6686136184bc29c3c27
Markus Kramer <markus.kramer@datalisk.com>
2024-07-10 12:39:46 +0200

    run code - wip

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ const af = require("./dist/build/S3ArtifactFactory");
 (async function() {
     await af.executeCommand("/home/smartstreamtv/workspace/ipy-scheduler/portal", "npm run build");
 })().catch(console.error);
+
+
+// TODO only if needed, in pulumi run phase
+// const portalArtifact = await pat.build.buildLocally({
+//     artifact: portalArtifactStore.getArtifactVersion("manual"),
+//     //workingDir: `${__dirname}/portal`,
+//     command: "npm run build",
+//     outputDir: "./build"
+// });
