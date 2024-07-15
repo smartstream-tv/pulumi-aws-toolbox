4651eca14d0ec75903de14969e8c193784bb573b
Markus Kramer <markus.kramer@datalisk.com~>
2024-07-15 22:35:48 +0200

    fix: aws login failing gracefully

--- bin/pulumi-aws-login.sh
+++ bin/pulumi-aws-login.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-set -e
 
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION=$(aws configure get region)
+  if [ -z "$AWS_REGION" ]; then
+    echo "AWS region could not be retrieved from AWS CLI configuration."
+    exit 1
+  fi
+fi
+
+set -e
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-AWS_REGION=$(aws configure get region)
 BUCKET="pulumi-state-${AWS_REGION}-${AWS_ACCOUNT_ID}"
 
 mkdir -p ~/.pulumi
@@ -19,3 +26,5 @@ cat >~/.pulumi/credentials.json <<EOL
     }
 }
 EOL
+
+echo Configured Pulumi to use bucket s3://$BUCKET
