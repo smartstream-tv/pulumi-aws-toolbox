7ebc29eabb25c83d743d9f5a40069bfa4bc30451
Markus Kramer <markus.kramer@datalisk.com~>
2024-07-16 19:07:57 +0200

    chore: prepared CI test execution

--- src/build/version.spec.ts
+++ src/build/version.spec.ts
@@ -4,6 +4,6 @@ test('getVersion wrong dir', async () => {
     await expect(() => getVersion('doesnotexist')).rejects.toThrow();
 });
 
-test('getVersion src/database', async () => {
+test('getVersion for src/database', async () => {
     expect(await getVersion('src/database')).toBe(`b5461773`);
 });
